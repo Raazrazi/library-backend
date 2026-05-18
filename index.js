44f5a1379d14c34c27ca8c0fad7eab7639f7ca4b
Raazrazi <i.students37dia@gmail.com>
2026-05-18 21:42:12 +0530

    Updated

--- index.js
+++ index.js
@@ -231,8 +231,6 @@ app.get('/api/history', async (req, res) => {
 });
 
 // CHANGE THIS PART:
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
